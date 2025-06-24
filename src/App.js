af31dc4cc778859e8278473240638ad647f2b324
Manbearjl <manbearjl@gmail.com>
2025-06-24 09:24:49 -0500

    Initial commit for deployment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Manitex 50155S Configuration Tool</h1>
+      <p>This is a placeholder for your crane configuration interface.</p>
+    </div>
+  );
+}
+
+export default App;
