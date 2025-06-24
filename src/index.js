93b83e80de0343c80bfae92ad645f0325476491c
Manbearjl <manbearjl@gmail.com>
2025-06-24 08:25:23 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Manitex50155STool from "./Manitex50155STool";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Manitex50155STool />
+  </React.StrictMode>
+);
